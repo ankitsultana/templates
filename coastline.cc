16915926ce468b6871f4f174ca509f6934ddb48b
bk2dcradle <ankitsultana@gmail.com>
2016-06-12 00:23:54 +0530

    add coastline + updates to dynamic array

--- coastline.cc
+++ coastline.cc
@@ -1,61 +1,70 @@
-/* Coastline */
-struct point {
-	LL x, y;
-	int idx;
-	bool operator<(const point &other) const {
-		if(x != other.x) {
-			return x < other.x;
-		}
-		if(y != other.y) {
-			return y > other.y;
-		}
-		return idx > other.idx;
-	}
-}points[MAXN], temp_points[MAXN];
-set<point> coastline;
-
-bool inside(point x)
-{
-	set<point>::iterator it;
-	it = lower_bound(coastline.begin(), coastline.end(), x);
-	if(it == coastline.end()) {
-		return false;
-	}
-	return it->y >= x.y;
-}
-
-/* ----*
-       |
-	   |	outside
-	   -----*
-	inside	|
-			|
-			|
-			----*
-				|
-				|
-				|
-*/
-
-void myInsert(point x)
-{
-	if(coastline.empty()) {
-		coastline.insert(x);return ;
-	}
-	if(inside(x)) {
-		return ;
-	}
-	coastline.insert(x);
-	set<point>::iterator it = coastline.find(x), jt;
-	if(it == coastline.begin())	return ;
-	it--;
-	while(it->y <= x.y) {
-		if(it == coastline.begin()) {
-			coastline.erase(it); break;
-		}
-		jt = --it, it++;
-		coastline.erase(it);
-		it = jt;
-	}
-}
-/* Coastline end */
+#include <iostream>
+#include <set>
+using namespace std;
+// Coastline to keep track of points inside(strictly) coastline
+//     |
+//     |                Inside
+//     x-------|
+//             |
+//             |
+//             x-------|
+//                     |
+//                     x-------
+struct coastline {
+    struct pt { // Make this global
+        int x, y;
+        bool operator<(const pt &other) const {
+            return x == other.x ? y < other.y : x < other.x;
+        }
+    };
+    set<pt> s;
+    bool inside(int x, int y) {
+        if(s.empty())    return false;
+        auto it = s.insert({x, y});
+        if(it.second == false)  return false; // point already in coastline
+        if(it.first == s.begin()) {           // Least point, never inside
+            s.erase(it.first); return false;
+        }
+        auto jt = prev(it.first);
+        s.erase(it.first);
+        // Check with previous element
+        if(jt->x == x) {
+            if(jt == s.begin()) {
+                return false;
+            } else {
+                return prev(jt)->y < y;
+            }
+        }
+        return jt->y < y;
+    }
+    void insert(int x, int y) {
+        if(inside(x, y))    return ;
+        s.insert({x, y});
+        // Adjust coastline now
+        auto jt = s.find({x, y});
+        jt = next(jt);
+        while(jt != s.end()) {
+            auto temp = next(jt);
+            if(jt->y >= y) {
+                s.erase(jt);
+            } else {
+                break;
+            }
+            jt = temp;
+        }
+        jt = s.find({x, y});
+        // If current lies ON coastline
+        if(jt != s.begin() and prev(jt)->x == x) {
+            s.erase(jt);
+        } else if(jt != s.begin() and prev(jt)->y == y) {
+            s.erase(jt);
+        }
+    }
+    friend ostream& operator<<(ostream &os, coastline &crap) {
+        for(auto &elem: crap.s) {
+            os << "(" << elem.x << ", " << elem.y << ") ";
+        }
+        return os;
+    }
+    inline bool empty() { return s.empty(); }
+};
--- dynamicarray.cc
+++ dynamicarray.cc
@@ -2,15 +2,15 @@ class dynamicarray {
 public:
     struct node { // prior for heap ordering and data for bst ordering
         int prior, size;
-        int val, maxim, s_maxim;
+        int val, maxim;
+        bool lazy;
         node *l, *r;
-        node() {
-            maxim = 0;
-            s_maxim = 0;
+        node() : maxim(0), lazy(false) {
         }
     };
     typedef node* pnode;
     pnode ROOT;
+    dynamicarray() { }
     template<typename iter>
     dynamicarray(iter st, iter e) : ROOT(NULL) {
         iter temp;
@@ -23,14 +23,22 @@ public:
         pnode temp = new node();
         temp->val = val;
         temp->prior = rand(), temp->size = 1;
+        temp->lazy = false;
         temp->l = temp->r = NULL;
         reset(temp);
         return temp;
     }
     int sz(pnode t)      { return t ? t->size: 0;                   }
     void upd_sz(pnode t) { if(t) t->size = sz(t->l) + 1 + sz(t->r); }
-    void reset(pnode t)  {
-        if(t) t->maxim = t->val, t->s_maxim = 0;
+    void reset(pnode t)  { // What if t is leaf
+        if(t) t->maxim = t->val;
+    }
+    void propagate(pnode t) {
+        if(!t || !t->lazy)  return ;
+        // Perform some opreation like < swap(t->l, t->r) >
+        if(t->l)    t->l->lazy ^= true;
+        if(t->r)    t->r->lazy ^= true;
+        t->lazy = false;
     }
     void combine(pnode t, pnode l, pnode r) {
         if(l and r) {
@@ -41,42 +49,25 @@ public:
         }
     }
     void operation(pnode t) {
-        upd_sz(t);
         if(!t)  return ;
+        upd_sz(t);
         reset(t);
         combine(t, t->l, t->r);
     }
     void split(pnode t, pnode &l, pnode &r, int pos, int add = 0) {
         if(!t)  return void(l = r = NULL);
+        propagate(t);
         int curr_pos = add + sz(t->l);
         if(curr_pos <= pos) split(t->r, t->r, r, pos, curr_pos+1), l = t;
         else                split(t->l, l, t->l, pos, add), r = t;
         operation(t);
     }
     void merge(pnode &t, pnode l, pnode r) {
+        propagate(l), propagate(r);
         if(!l || !r) t = l ? l: r;
         else if(l->prior > r->prior) merge(l->r, l->r, r), t = l;
         else merge(r->l, l, r->l), t = r;
         operation(t);
-    }
-    pnode kth(pnode root, int k) {
-        if(!root->l and !root->r)   return root;
-        else if(!root->l || !root->r) {
-            if(!root->l) {
-                if(k == 1)  return root;
-                return kth(root->r, k-1);
-            } else {
-                if(root->l->size + 1 == k)  return root;
-                return kth(root->l, k);
-            }
-        } else if(root->l->size < k) {
-            k -= root->l->size + 1;
-            if(k == 0)  return root;
-            return kth(root->r, k);
-        } else {
-            return kth(root->l, k);
-        }
-        return NULL;
     }
     void erase(int idx) {
         pnode temp, l, r;
@@ -90,7 +81,7 @@ public:
         merge(ROOT, l, r);
         delete temp;
     }
-    void insert(int idx, int val) {
+    void insert(int idx, int val) { // Insert val before idx (zero based)
         pnode temp = get_new_node(val);
         if(idx == 0) {
             merge(ROOT, temp, ROOT);
@@ -105,10 +96,7 @@ public:
         pnode L, mid, R;
         split(t, L, mid, l - 1);
         split(mid, t, R, r - l);
-        /* cout << "===============\n";
-        preorder(t);
-        cout << "===============\n"; */
-        int res = t->maxim + t->s_maxim;
+        int res = t->maxim;
         merge(mid, L, t);
         merge(t, mid, R);
         return res;
@@ -127,25 +115,18 @@ public:
         preorder(t->l);
         preorder(t->r);
     }
-};
-
-int arr[MAXN];
-int main() {
-    int n, q, a, b, t;
-    char opt;
-    int c;
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++)  scanf("%d", &arr[i]);
-    dynamicarray tester(arr, arr + n);
-    scanf("%d", &q);
-    while(q--) {
-        scanf("\n%c", &opt);
-        scanf("%d %d", &a, &b);
-        if(opt == 'Q') {
-            printf("%d\n", tester.query(tester.ROOT, a-1, b-1));
+    void reversal(int a, int b) {
+        pnode l, r, mid;
+        l = r = mid = NULL;
+        split(ROOT, l, r, b);
+        if(a == 0) { // l contains the shit
+            l->lazy ^= true;
+            merge(ROOT, l, r);
         } else {
-            tester.update(tester.ROOT, a-1, a-1, b);
+            split(l, l, mid, a-1);
+            mid->lazy ^= true;
+            merge(l, l, mid);
+            merge(ROOT, l, r);
         }
     }
-    return 0;
-}
+};
