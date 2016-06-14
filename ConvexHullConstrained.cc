53a9a2d7c9d7689f374e767603fc944462a1c79a
bk2dcradle <ankitsultana@gmail.com>
2016-06-14 20:28:26 +0530

    new convex hull

--- ConvexHullConstrained.cc
+++ ConvexHullConstrained.cc
@@ -1,57 +1,48 @@
-/* Start Convex Hull (Constrained) */
-#define MAXN 1000003
-struct Line {
-	LL m, c;
-	int idx;
-}lines[MAXN];
+#include <iostream>
+#include <vector>
+using namespace std;
 
-LL dp[MAXN];
-double stack[MAXN];
-int line_no[MAXN] = {0};
+/*
+ * Convex Hull Constrained
+ * Constrain being that the slopes should be strictly increasing/decreasing
+ */
 
+template<typename T>
 struct HullConstrained {
+    struct Line {
+        T m, c;
+        Line() { }
+        Line(T m, T c) : m(m), c(c) { }
+    };
+    HullConstrained() { }
+    HullConstrained(int sz) {
+        pts.reserve(sz), lines.reserve(sz);
+    }
+    void insert(Line x) {
+        if(lines.size() <= 1) {
+            lines.push_back(x);
+            if(lines.size() == 2) {
+                pts.push_back(get_x(lines[0], lines[1]));
+            }
+            return ;
+        }
+        double curr_x = get_x(x, lines.back());
+        while(pts.size() > 0 and pts.back() >= curr_x) {
+            lines.pop_back(), pts.pop_back();
+            curr_x = get_x(x, lines.back());
+        }
+        lines.push_back(x);
+        if(lines.size() > 1) pts.push_back(curr_x);
+    }
+    T eval(T x) {
+        int idx = int(lower_bound(pts.begin(), pts.end(), x) - pts.begin());
+        return lines[idx].c + lines[idx].m * x;
+    }
+    inline bool empty() { return lines.empty(); }
 private:
-	bool upper;
-	int pnt_cnt, line_cnt;
-public:
-	HullConstrained(string upper) {
-		this->upper = upper == "upper";
-		pnt_cnt = 0, line_cnt = 0;
-	}
-	void insert(Line x) {
-		if(line_cnt == 0) {
-			line_no[0] = x.idx, line_cnt++;return ;
-		}
-		if(line_cnt == 1) {
-			if(lines[line_no[0]].m == x.m) {
-				if(upper && lines[line_no[0]].c < x.c)
-					line_no[0] = x.idx;
-				else if(!upper && lines[line_no[0]].c > x.c)
-					line_no[0] = x.idx;
-			} else {
-				line_no[1] = x.idx, line_cnt++;
-				stack[0] = get_x(lines[line_no[0]], lines[line_no[1]]);
-				pnt_cnt++;
-			}
-			return ;
-		}
-		double x1 = get_x(lines[line_no[pnt_cnt-1]], lines[x.idx]), x2 = stack[pnt_cnt-1];
-		while(pnt_cnt > 0 && x1 <= x2) {
-			x2 = stack[pnt_cnt-1]; 
-			x1 = get_x(lines[line_no[pnt_cnt-1]], lines[x.idx]);;  
-			if(x1 > x2)	break;
-			pnt_cnt--, line_cnt--;
-		}
-		pnt_cnt++, line_cnt++;
-		stack[pnt_cnt-1] = get_x(lines[line_no[pnt_cnt-1]], lines[x.idx]);
-		line_no[pnt_cnt] = x.idx;
-	}
-	double get_x(Line &l, Line &r) {
-		return double(r.c - l.c)/double(l.m - r.m);
-	}
-	LL eval(LL curr_x) {
-		int idx = int(lower_bound(stack, stack + pnt_cnt, double(curr_x)) - stack);
-		return lines[line_no[idx]].c + lines[line_no[idx]].m*(LL)curr_x;
-	}
+    vector<double> pts;
+    vector<Line> lines;
+    double get_x(Line &l, Line &r) {
+        return double(l.c - r.c) / double(r.m - l.m);
+    }
 };
-/* End */
