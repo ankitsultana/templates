4b3b805e33fb855875c4218b04e4c98cb423cc77
bk2dcradle <ankitsultana@gmail.com>
2016-07-07 17:14:39 +0530

    updates

--- sparse_table.cc
+++ sparse_table.cc
@@ -6,6 +6,7 @@ using namespace std;
 template<typename T, typename Compare = std::less<T> >
 struct sparse_table {
     int n;
+    Compare cmp;
     vector<vector<T> > dp;
     template<typename iter>
     void build(iter a, iter b) {
@@ -17,17 +18,17 @@ struct sparse_table {
         for(i = 0; i < size; i++, temp++) dp[i][0] = *temp;
         for(i = 1; i <= l; i++) {
             upper = size - (1<<(i-1));
-            for(j = 0; j < upper; j++) dp[j][i] = __min(dp[j][i-1], dp[j + (1<<(i-1))][i-1]);
+            for(j = 0; j < upper; j++)
+                dp[j][i] = cmp(dp[j][i-1], dp[j + (1<<(i-1))][i-1]) ? dp[j][i-1] : dp[j + (1<<(i-1))][i-1];
             for(; j < size; j++)    dp[j][i] = dp[j][i-1];
         }
     }
     T query(int i, int j) { // return min/max in range i..j (0 based)
         if(i == j)  return dp[i][0];
         int l = __log2(j - i);
-        return __min(dp[i][l], dp[j - (1<<l) + 1][l]);
+        return cmp(dp[i][l], dp[j - (1<<l) + 1][l]) ? dp[i][l] : dp[j - (1<<l) + 1][l];;
     }
 private:
     const int tab32[32] = {0,9,1,10,13,21,2,29,11,14,16,18,22,25,3,30,8,12,20,28,15,17,24,7,19,27,23,6,26,5,4,31};
     int __log2(int x) { return x |= x >> 1, x |= x >> 2, x |= x >> 4, x |= x >> 8, x |= x >> 16, tab32[(uint32_t)(x*0x07C4ACDD) >> 27]; }
-    inline int __min(int a, int b) { return b + ((a-b) & (a-b)>>31); }
 };
