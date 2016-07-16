aa00658aeca619a375475cb2b1494758e91084da
bk2dcradle <ankitsultana@gmail.com>
2016-07-16 16:54:29 +0530

    add codechef stack trick

--- UnionFind.cc
+++ UnionFind.cc
@@ -1,23 +1,23 @@
-class UnionFind{
-	private:
-		int V, Rank[MAXN], P[MAXN];
-	public:
-		UnionFind(int x) {
-			V = x;
-			for(int i = 0; i <= x; i++)
-				Rank[i] = 1, P[i] = i;
-		}
-		int find_root(int x) {
-			if(x == P[x])	return x;
-			return P[x] = find_root(P[x]);
-		}
-		void Union(int x, int y) {
-			int xr = find_root(P[x]), yr = find_root(P[y]);
-			if(xr == yr)
-				return ;
-			if(Rank[xr] >= Rank[yr])
-				Rank[xr] += Rank[yr], P[yr] = xr;
-			else
-				Rank[yr] += Rank[xr], P[xr] = yr;
-		}
+class DSU {
+private:
+    int V, Rank[MAXN], P[MAXN];
+public:
+    DSU(int x) {
+        V = x;
+        for(int i = 0; i <= x; i++)
+            Rank[i] = 1, P[i] = i;
+    }
+    int find_root(int x) {
+        if(x == P[x])	return x;
+        return P[x] = find_root(P[x]);
+    }
+    void Union(int x, int y) {
+        int xr = find_root(P[x]), yr = find_root(P[y]);
+        if(xr == yr)
+            return ;
+        if(Rank[xr] >= Rank[yr])
+            Rank[xr] += Rank[yr], P[yr] = xr;
+        else
+            Rank[yr] += Rank[xr], P[xr] = yr;
+    }
 };
---
+++ codechef_stack.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cassert>
+#include <sys/resource.h>
+using namespace std;
+int main() {
+    const rlim_t Stack_size = 32L * 1024 * 1024; // At least 32 Mb Stack
+    struct rlimit lim = {Stack_size, Stack_size};
+    if(setrlimit(RLIMIT_STACK, &lim) == -1) assert(false);
+    return 0;
+}
